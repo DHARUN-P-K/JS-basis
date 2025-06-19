31e9e2a9a25170df2186e732990fc262c8f10cde
DHARUN_P_K <dharunpk.23eie@kongu.edu>
2025-06-19 10:53:25 +0530

    after complete operators

---
+++ float.js
@@ -0,0 +1,9 @@
+let n = 0.8
+let o = 8.2
+console.log((n+o).toFixed(4))
+let mme = Math.round(80+(93+97)/2);
+console.log(mme)
+console.log(10>2);
+console.log("2"===2);
+console.log(5>2 && 1<6);
+console.log("b">"a");
