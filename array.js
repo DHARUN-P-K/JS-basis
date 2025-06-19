598c5e96ca1fe256ee91b56abbb589c1b7f576f0
DHARUN_P_K <dharunpk.23eie@kongu.edu>
2025-06-19 12:03:04 +0530

    arrary cmp

---
+++ array.js
@@ -0,0 +1,25 @@
+let fruits = ["apple","mango","grapes"];
+console.log("original array");
+console.log(fruits);
+
+fruits.pop();
+console.log("after pop");
+console.log(fruits);
+
+fruits.push("green");
+console.log("after push");
+console.log(fruits);
+
+console.log(fruits.length);
+console.log(fruits[1]);
+
+fruits.unshift("bell");
+console.log(fruits)
+
+fruits.shift()
+console.log(fruits)
+
+
+fruits.slice(1,1)
+console.log(fruits)
+
