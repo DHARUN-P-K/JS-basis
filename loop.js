7f02004e8ebe9507ec99122e0e82c8908fc476aa
DHARUN_P_K <dharunpk.23eie@kongu.edu>
2025-06-19 11:37:37 +0530

    complete loop

---
+++ loop.js
@@ -0,0 +1,19 @@
+// for(let i=100;i>0;i--){
+//     console.log(i);
+// }
+console.log("break");
+let a = 100;
+while(a>10){
+    if(a===50){
+        a--;
+        continue
+    }
+    if(a===30){
+        break;
+    }
+    console.log(a);
+    a--;
+    
+}
+
+    // if loop continuous use ctrl c
