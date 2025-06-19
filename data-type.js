ed90d096875ee3246269e8d343cb626cbb5e799b
DHARUN_P_K <dharunpk.23eie@kongu.edu>
2025-06-19 10:02:04 +0530

    complete data types in java script

---
+++ data-type.js
@@ -0,0 +1,14 @@
+console.log("hello i am new to java script");
+console.log("2"+2);
+console.log("2"-2);
+console.log("two"+2);
+let am = "welcome";
+let m = "money";
+console.log(am+m);
+for(let i =0;i<21;i++){
+    console.log(i,"\t");
+}
+let b=5;
+let r= 6;
+console.log(b+r);
+
