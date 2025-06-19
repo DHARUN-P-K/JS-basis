d86f4461d90478ef4ac75392677e4400bd68c605
DHARUN_P_K <dharunpk.23eie@kongu.edu>
2025-06-19 11:21:48 +0530

    complete if else

---
+++ conditionals.js
@@ -0,0 +1,20 @@
+
+
+let marks = prompt("Enter your marks:");
+marks = Number(marks); // convert input from string to number
+
+if(marks >= 65){
+    console.log("pass");
+}
+else{
+    console.log("fail");
+}
+let mar = 63;
+if(mar>=65){
+    console.log("pass");
+}
+else{
+    console.log("fail");
+}
+
+
